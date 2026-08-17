dfb6235931a6138bccb4909befe650b703bf1f6a
deepakkushwaha78 <deepak.kushwaha@opstree.com>
2026-08-17 13:16:45 +0530

    Add Azure RM backend configuration for Terraform

---
+++ acr/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "buildpiper"
+    storage_account_name = "terraformstatebp"
+    container_name       = "state-file"
+    key                  = "env/dev/acr/terraform.tfstate"
+  }
+}
